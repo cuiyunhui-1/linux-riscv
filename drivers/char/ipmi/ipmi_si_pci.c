c-sky/csky-linux

driver: ipmi: Add sophgo sg2042 support
exchange messages and data between the AST2600 BMC and the Host SG2042
over PCIe link using BAR1. Have to set the KCS channel offset is
0x0e80 according to the AST2600 User Guide.

Signed-off-by: Xiaoguang Xing <xiaoguang.xing@sophgo.com>

drivers/char/ipmi/ipmi_si_pci.c
@@ -111,6 +111,12 @@ static int ipmi_pci_probe(struct pci_dev *pdev,
 	io.regsize = DEFAULT_REGSIZE;
 	io.regshift = 0;
 
+#ifdef CONFIG_SOC_SOPHGO
+	io.addr_data = pci_resource_start(pdev, 1) + 0x0e80;
+	io.slave_addr = 0x20;
+	io.regspacing = 4;
+	io.regsize = 4;
+#endif
 	io.irq = pdev->irq;
 	if (io.irq)
 		io.irq_setup = ipmi_std_irq_setup;

cuiyunhui-1/linux-riscv

drivers/char/ipmi/ipmi_si_pci.c
@@ -111,6 +111,12 @@ static int ipmi_pci_probe(struct pci_dev *pdev,
 	io.regsize = DEFAULT_REGSIZE;
 	io.regshift = 0;
 
+#ifdef CONFIG_ARCH_SOPHGO
+	io.addr_data = pci_resource_start(pdev, 1) + 0x0e80;
+	io.slave_addr = 0x20;
+	io.regspacing = 4;
+	io.regsize = 4;
+#endif
 	io.irq = pdev->irq;
 	if (io.irq)
 		io.irq_setup = ipmi_std_irq_setup;